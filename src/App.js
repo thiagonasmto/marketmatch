e35391471a9ffbe43e2a5f78008a1d931f306449
Thiago Lopes do Nascimento <thiagonasmento20@gmail.com>
2024-08-07 15:51:23 -0300

    bugfix: Ajustando a responsividade das telas

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import React from 'react';
 import Home from './telas/Home'
 import Itens from './telas/Itens'
+import MatchPage from './telas/MatchPage';
+import ContactPage from './telas/ContactPage';
 
 function App() {
   return (
-    <Itens />
+    <MatchPage />
   );
 }
 
